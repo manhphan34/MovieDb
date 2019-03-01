3cd7067d56e7f3fcfbcccd3ee9c5bb1d28a732cc
phanmanh <manhphan341996@gmail.com>
2019-03-01 15:17:24 +0700

    fix comment 2

--- app/src/main/java/com/example/moviedb/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/example/moviedb/screen/home/HomeViewModel.kt
@@ -4,13 +4,12 @@ import androidx.lifecycle.MutableLiveData
 import com.example.moviedb.base.BaseViewModel
 import com.example.moviedb.data.model.Movie
 import com.example.moviedb.data.repository.MovieRepository
-import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
 class HomeViewModel(private val repository: MovieRepository) : BaseViewModel() {
     val movies: MutableLiveData<List<Movie>> = MutableLiveData()
+    val error: MutableLiveData<String> = MutableLiveData()
 
     fun loadData(page: Int) {
         val disposable = repository.getMovies(page).observeOn(AndroidSchedulers.mainThread())
@@ -18,7 +17,7 @@ class HomeViewModel(private val repository: MovieRepository) : BaseViewModel() {
             .subscribe({
                 movies.value = it
             }, {
-                //do something
+                error.value = it.message
             })
         launchDisposable { disposable }
     }
